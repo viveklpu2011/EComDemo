410b9a30aabfdc9cc40f2d902af0d122ef565dce
viveklpu2011 <viveklpu2011@gmail.com>
2020-10-07 13:53:26 +0530

    missing files

---
+++ EComDemo/Controls/ColumnListView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace EComDemo.Controls
+{
+    public partial class ColumnListView : ContentView
+    {
+        public ColumnListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
